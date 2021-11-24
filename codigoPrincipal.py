d4288b1d8bf0a41360b23436eeaab0d9b370a941
HenriBDev <henribdev@gmail.com>
2021-11-23 21:15:42 -0300

    :sparkles: UI, testes manuais e exportação
    - Adicionado menu do sistema
    - Adicionada opção de realizar teste pré-definido ou teste com um valor selecionado de itens na list
    - Adicionada opção de exportar os resultados em .xlsx ou .csv

--- codigoPrincipal.py
+++ codigoPrincipal.py
@@ -1,11 +1,14 @@
 from random import seed
 from random import randint
 import time
+from datetime import datetime
 import sys
+import os
 import numpy as np
 import pandas as pd
+from pandas.io.sql import pandasSQL_builder
  
-def quickSortIterative(colecao, l, h):
+def quickSortIterable(colecao, l, h):
  
     size = h - l + 1
     stack = [0] * (size)
@@ -65,41 +68,111 @@ def createCollection(size):
         collection.append(value)
     return collection
 
-def testPerformance(multiplyBy5, collectionSize, recursiveResults = [], iterativeResults = [], sizesCollection = []):
-    if collectionSize <= 10000:
-
-        print(f"Testando lista com {collectionSize} itens. . .")
-        
-        sizesCollection.append(collectionSize)
-        collection = createCollection(collectionSize)
-
-        initialTime = time.time() 
-        quickSortRecursive(collection, 0, collectionSize - 1)
-        finalTime = time.time()
-        recursiveResults.append(f"{round(finalTime - initialTime,4)}s")
-
-        initialTime = time.time() 
-        quickSortIterative(collection, 0, collectionSize - 1) 
-        finalTime = time.time()
-        iterativeResults.append(f"{round(finalTime - initialTime,4)}s")
-
-        multiplyBy5 = not multiplyBy5
-        if multiplyBy5:
-            collectionSize *= 5
-        else:
-            collectionSize *= 2
-
-        testPerformance(multiplyBy5, collectionSize, recursiveResults, iterativeResults)
-
+def predefinedTest(resultsDict, multiplyBy2 = True, collectionSize = 5):
+    
+    if multiplyBy2:
+        collectionSize *= 2
     else:
-        print("Ok!\n\nVeja os resultados:\n")
+        collectionSize *= 5
+    multiplyBy2 = not multiplyBy2
 
-        dictResults = {"TAMANHO_LISTA": sizesCollection, "TEMPO_RECURSIVA": recursiveResults, "TEMPO_ITERATIVA": iterativeResults}
-        dataFrame_Results = pd.DataFrame.from_dict(dictResults)
-        print(dataFrame_Results)
+    if (collectionSize <= 10000):
+        resultsDict = testPerformance(collectionSize, resultsDict)
+        return predefinedTest(resultsDict, multiplyBy2, collectionSize)
+    else:
+        return resultsDict
+
+def testPerformance(collectionSize, resultsDict):
+
+    print(f"Testando lista com {collectionSize} itens. . .")
+
+    resultsDict["MOMENTO_REALIZADO"].append(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    
+    resultsDict["TAMANHO_LISTA"].append(collectionSize)
+    collection = createCollection(collectionSize)
+
+    initialTime = time.time() 
+    quickSortRecursive(collection, 0, collectionSize - 1)
+    finalTime = time.time()
+    resultsDict["TEMPO_RECURSIVA"].append(f"{round(finalTime - initialTime,4)}s")
+
+    initialTime = time.time() 
+    quickSortIterable(collection, 0, collectionSize - 1) 
+    finalTime = time.time()
+    resultsDict["TEMPO_ITERAVEL"].append(f"{round(finalTime - initialTime,4)}s")
+
+    return resultsDict
+
+def showResults(resultsDict):
+    print("Veja os resultados:\n")
+    print(pd.DataFrame.from_dict(resultsDict))
+    input("\nPressione enter para continuar. . .")
 
 def main():
-    testPerformance(False, 10)
+
+    resultsDict = {"MOMENTO_REALIZADO": [],
+                   "TAMANHO_LISTA": [],
+                   "TEMPO_ITERAVEL": [],
+                   "TEMPO_RECURSIVA": []}
+
+    print("Bem-vindo ao sistema de teste de perfomance: Recursividade X Iteração\n" +
+          "Neste teste será executado duas funções Quicksort em uma list contendo diversos itens, uma delas percorre a list de forma recursiva, enquanto a outra faz isso de forma iterativa\n" +
+          "Os resultados serão relatados após o teste")
+
+    while True:
+        os.system("CLS")
+        print("1 - Realizar teste pré-definido (10 até 10000 itens)\n" +
+              "2 - Realizar teste com um número selecionado de itens\n" +
+              "3 - Ver resultado dos testes\n" +
+              "4 - Exportar resultados em uma planilha (.xlsx)\n" +
+              "5 - Exportar resultados em um arquivo de texto (.csv)\n" +
+              "6 - Limpar resultados\n" +
+              "7 - Sair\n")
+
+        selection = int(input("Escolha: "))
+        if (selection < 1 or selection > 7):
+            print("Escolha inválida! Selecione um número entre 1 e 7")
+            input("Pressione enter para continuar. . .")
+        else:
+            os.system("CLS")
+            if (selection == 1):
+                resultsDict = predefinedTest(resultsDict)
+                showResults(resultsDict)
+            elif (selection == 2):
+                qtyItems = int(input("Digite a quantidade de itens da list: "))
+                if (sys.getrecursionlimit() < qtyItems):
+                    sys.setrecursionlimit(qtyItems)
+                resultsDict = testPerformance(qtyItems, resultsDict)
+                showResults(resultsDict)
+            elif (selection == 3):
+                if(resultsDict["TAMANHO_LISTA"] != []):
+                    showResults(resultsDict)
+                else:
+                    input("Ainda não foi realizado nenhum teste.\nPressione enter para continuar. . .")
+            elif (selection == 4):
+                resultsDataFrame = pd.DataFrame.from_dict(resultsDict)
+                resultsDataFrame.to_excel(f"{input('Digite o nome do arquivo: ')}.xlsx")
+                input("Arquivo gerado com sucesso!\nPressione Enter Para Continuar. . .")
+            elif (selection == 5):
+                resultsDataFrame = pd.DataFrame.from_dict(resultsDict)
+                resultsDataFrame.to_csv(f"{input('Digite o nome do arquivo: ')}.csv")
+                input("Arquivo gerado com sucesso!\nPressione Enter Para Continuar. . .")
+            elif (selection == 6):
+                while True:
+                    confirm = input("Tem certeza que deseja apagar os resultados? [S/N]\nEscolha: ").upper()
+                    os.system("CLS")
+                    if (confirm == "S" or confirm == "N"):
+                        if (confirm == "S"):
+                            resultsDict = {"MOMENTO_REALIZADO": [],
+                                           "TAMANHO_LISTA": [],
+                                           "TEMPO_RECURSIVA": [],
+                                           "TEMPO_ITERAVEL": []}
+                            input("Dados apagados com sucesso!\nPressione enter para continuar...")
+                        break
+            else:
+                break
+
+    print("Obrigado por usar o sistema!")
     
 if  __name__ == '__main__' :
     main()
